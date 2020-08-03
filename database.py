8af45ce720122c3160ef784b1ef2d1da1132541f
Philipe Goulet <phil.gou69@hotmail.com>
2020-08-03 19:19:50 -0400

    Added support for passiveChannels

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-
+import itertools
 
 ActiveChannelType = 1
 TemperatureChannelType = 2
@@ -7,29 +7,11 @@ sensorDataTable = "SensorData"
 
 import mysql.connector
 
+class PassiveChannel():
 
-class TemperatureChannel():
-
-    channelType = TemperatureChannelType
-
-    def __init__(self, channelID, sensorID = 0):
+    def __init__(self, channelID, channelType, voltageSensorID = 0, currentSensorID = 0):
         self.channelID = channelID
-        self.sensorID = sensorID
-
-    def sendUpdate(self, cursor, time, temperature):
-        sql = "INSERT INTO " + sensorDataTable + "(time, SensorID, Data) VALUES (%s, %s, %s)"
-        val = (time, self.sensorID, temperature)
-        cursor.execute(sql,val)
-
-    def printIDs(self):
-        print("channelID: " + str(self.channelID) + "| sensorID: " + str(self.sensorID))
-
-class ActiveChannel():
-
-    channelType = ActiveChannelType
-
-    def __init__(self, channelID, voltageSensorID = 0, currentSensorID = 0):
-        self.channelID = channelID
+        self.channelType = channelType
         self.voltageSensorID = voltageSensorID
         self.currentSensorID = currentSensorID
     
@@ -43,10 +25,31 @@ class ActiveChannel():
     def printIDs(self):
         print("channelID: " + str(self.channelID) + "| voltageID: " + str(self.voltageSensorID) + "| currentID: " + str(self.currentSensorID))
 
+
+
+class TemperatureChannel():
+
+    def __init__(self, channelID, channelType, sensorID = 0):
+        self.channelID = channelID
+        self.channelType = channelType
+        self.sensorID = sensorID
+
+    def sendUpdate(self, cursor, time, temperature):
+        sql = "INSERT INTO " + sensorDataTable + "(time, SensorID, Data) VALUES (%s, %s, %s)"
+        val = (time, self.sensorID, temperature)
+        cursor.execute(sql,val)
+
+    def printIDs(self):
+        print("channelID: " + str(self.channelID) + "| sensorID: " + str(self.sensorID))
+
+class ActiveChannel(PassiveChannel):
+    pass
+
 class DataBase():
 
     ActiveChannels = []
     TemperatureChannels = []
+    PassiveChannels = []
     
     def __init__(self):
         self.db = mysql.connector.connect(
@@ -59,11 +62,22 @@ class DataBase():
         self.cursor.execute("SELECT ChannelID, ChannelType FROM Channels")
         Channels = self.cursor.fetchall()
 
+        self.cursor.execute("SELECT ChannelType, Name FROM ChannelTypes")
+        ChannelTypes = self.cursor.fetchall()
+
         self.cursor.execute("SELECT SensorID, SensorType, ChannelID FROM Sensors")
         Sensors = self.cursor.fetchall()
 
         self.cursor.execute("SELECT SensorType, SensorDataName FROM SensorTypes")
         SensorTypes = self.cursor.fetchall()
+
+        for channelType in ChannelTypes:
+            if channelType[1] == "ActiveChannel":
+                self.ActiveChannelType = channelType[0]
+            elif channelType[1] == "TemperatureChannel":
+                self.TemperatureChannelType = channelType[0]
+            elif channelType[1] == "PassiveChannel":
+                self.PassiveChannelType = channelType[0]
 
         for sensorType in SensorTypes:
             if sensorType[1] == "voltage":
@@ -74,22 +88,33 @@ class DataBase():
                 self.temperatureSensorType = sensorType[0]
 
         for channel in Channels:
-            if channel[1] == ActiveChannelType:
-                self.ActiveChannels.append(ActiveChannel(channel[0]))
-            if channel[1] == TemperatureChannelType:
-                self.TemperatureChannels.append(TemperatureChannel(channel[0]))
+            if channel[1] == self.ActiveChannelType:
+                self.ActiveChannels.append(ActiveChannel(channel[0], self.ActiveChannelType))
+            if channel[1] == self.TemperatureChannelType:
+                self.TemperatureChannels.append(TemperatureChannel(channel[0], self.TemperatureChannelType))
+            if channel[1] == self.PassiveChannelType:
+                self.PassiveChannels.append(PassiveChannel(channel[0], self.PassiveChannelType))
 
         for sensor in Sensors:
-            for channel in self.ActiveChannels:
-                if channel.channelID == sensor[2]:
+            for passiveChannel in self.PassiveChannels:
+                if passiveChannel.channelID == sensor[2]:
                     if sensor[1] == self.voltageSensorType:
-                        channel.voltageSensorID = sensor[0]
+                        passiveChannel.voltageSensorID = sensor[0]
                     elif sensor[1] == self.currentSensorType:
-                        channel.currentSensorID = sensor[0]
-            for channel in self.TemperatureChannels:
-                if channel.channelID == sensor[2]:
+                        passiveChannel.currentSensorID = sensor[0]
+                    break
+            for activeChannel in self.ActiveChannels:
+                if activeChannel.channelID == sensor[2]:
+                    if sensor[1] == self.voltageSensorType:
+                        activeChannel.voltageSensorID = sensor[0]
+                    elif sensor[1] == self.currentSensorType:
+                        activeChannel.currentSensorID = sensor[0]
+                    break
+            for temperatureChannel in self.TemperatureChannels:
+                if temperatureChannel.channelID == sensor[2]:
                     if sensor[1] == self.temperatureSensorType:
-                        channel.sensorID = sensor[0]
+                        temperatureChannel.sensorID = sensor[0]
+                    break
 
         for channel in self.ActiveChannels:
             channel.printIDs()
